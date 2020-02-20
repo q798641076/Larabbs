2d4cb9a6bfaefb1abb3670dce817b95fa268e598
vagrant <vagrant@homestead>
2020-02-20 13:00:25 +0000

    修复跳转链接，引入form组件

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -16,9 +16,32 @@
 
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
+        @guest
         <!-- Authentication Links -->
         <li class="nav-item"><a class="nav-link" href="{{route('login')}}">登录</a></li>
         <li class="nav-item"><a class="nav-link" href="{{route('register')}}">注册</a></li>
+        @else
+         <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+            class="img-responsive img-circle" width="30px" height="30px">
+            {{Auth::user()->name}}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="" class="dropdown-item">个人中心</a>
+            <a href="" class="dropdown-item">编辑资料</a>
+            <div class="dropdown-divider"></div>
+            <a href="" class="dropdown-item">
+           {!! Form::open(['route'=>'logout','method'=>'post']) !!}
+              <button class="btn btn-danger btn-block " type="submit">退出</button>
+           {!! Form::close() !!}
+            </a>
+
+         </div>
+        </li>
+        @endguest
+
       </ul>
     </div>
   </div>
