2e82d2fea417ab0df485b6ab972e0e9ca1e806e8
vagrant <vagrant@homestead>
2020-02-20 17:01:24 +0000

    邮箱认证

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    //要use MustVerifyEmailTrait才能用到里面的方法
+    use Notifiable , MustVerifyEmailTrait;
 
     /**
      * The attributes that are mass assignable.
